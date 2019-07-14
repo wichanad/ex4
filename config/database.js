635438276d61ceac553da05591bb94831bed38b2
wichanad <59143434@g.cmru.ac.th>
2019-07-14 11:51:49 +0700

    ก่อนเพิ่มหน้าคี

---
+++ config/database.js
@@ -0,0 +1,10 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'todo'
+});
+
+connection.connect();
+module.exports= connection
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+router.get("/play/:num1/:num2", function(req, res, next) {
+  //console.log(req.params)
+  var num1 = req.params.num1;
+  var num2 = req.params.num2;
+  var sum = parseInt(num1) + parseInt(num2);
+  res.send("ผลรวมของตัวเลข 2 จำนวนคือ " + sum);
+ });
+ 
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create', { title: 'Express' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
 });
 
 module.exports = router;
